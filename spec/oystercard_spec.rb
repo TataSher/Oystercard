b87be0f47fcc8df3ecb14288fdcc5a0b7a3b3b70
Natalia_Sherchenkova <tataterer@gmail.com>
2021-03-08 17:26:15 +0200

    complete challenge #03 & #4 completed. Add initialize method with zero balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,15 @@
+require './lib/oystercard.rb'
+
 describe Oystercard do
-  
+  it 'to have a default balance of 0' do
+    expect(subject).to respond_to(:balance)
+  end
+
+  it 'to have default balance as 0' do
+    expect(subject.balance).to eq(0)
+  end
 end
+
+# oystercard = Oystercard.new
+# afglakjfdg;lkdjfg
+#
