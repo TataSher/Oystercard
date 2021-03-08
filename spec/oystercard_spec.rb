abc8321b8b287366c2c059a8d6a10573bb3b5524
Natalia_Sherchenkova <tataterer@gmail.com>
2021-03-08 18:07:41 +0200

    complete step #5. Card can be topped up

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,15 +1,19 @@
 require './lib/oystercard.rb'
 
 describe Oystercard do
-  it 'to have a default balance of 0' do
+  it 'to have a balance' do
     expect(subject).to respond_to(:balance)
   end
 
   it 'to have default balance as 0' do
     expect(subject.balance).to eq(0)
   end
-end
 
-# oystercard = Oystercard.new
-# afglakjfdg;lkdjfg
-#
+  it 'can be topped up' do
+    expect(subject).to respond_to(:top_up).with(1).argument
+  end
+
+  it 'top_up the original balance with new amount'do
+    expect { subject.top_up 1 }.to change { subject.balance }.by 1
+  end
+end
